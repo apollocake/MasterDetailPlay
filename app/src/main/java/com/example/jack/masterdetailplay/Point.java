b043addeff1aee21d31d4e8dca98e200cb724df8
Afton Martin <aftonam@gmail.com>
2016-12-08 15:19:05 -0300

    points added in a random generation pattern

--- app/src/main/java/com/example/jack/masterdetailplay/Point.java
+++ app/src/main/java/com/example/jack/masterdetailplay/Point.java
@@ -19,12 +19,10 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import android.opengl.GLES20;
 
-import com.example.jack.masterdetailplay.MyGLRenderer;
-
 /**
  * A two-dimensional triangle for use as a drawn object in OpenGL ES 2.0.
  */
-public class Triangle {
+public class Point {
     private final String vertexShaderCode =
             // This matrix member variable provides a hook to manipulate
             // the coordinates of the objects that use this vertex shader
@@ -35,6 +33,7 @@ public class Triangle {
                     // Note that the uMVPMatrix factor *must be first* in order
                     // for the matrix multiplication product to be correct.
                     "  gl_Position = uMVPMatrix * vPosition;" +
+                    "gl_PointSize = 20.0;" +
                     "}";
     private final String fragmentShaderCode =
             "precision mediump float;" +
@@ -49,29 +48,27 @@ public class Triangle {
     private int mMVPMatrixHandle;
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
-    static float triangleCoords[] = {
+    static float pointCoords[] = {
             // in counterclockwise order:
-            0.0f,  0.622008459f, 0.0f,   // top
-            -0.5f, -0.311004243f, 0.0f,   // bottom left
-            0.5f, -0.311004243f, 0.0f    // bottom right
+            0.0f,  0.622008459f, 0.0f,   // x,y,z
     };
-    private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
+    private final int vertexCount = pointCoords.length / COORDS_PER_VERTEX;
     private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
     float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 0.0f };
     /**
      * Sets up the drawing object data for use in an OpenGL ES context.
      */
-    public Triangle() {
+    public Point() {
         // initialize vertex byte buffer for shape coordinates
         ByteBuffer bb = ByteBuffer.allocateDirect(
                 // (number of coordinate values * 4 bytes per float)
-                triangleCoords.length * 4);
+                pointCoords.length * 4);
         // use the device hardware's native byte order
         bb.order(ByteOrder.nativeOrder());
         // create a floating point buffer from the ByteBuffer
         vertexBuffer = bb.asFloatBuffer();
         // add the coordinates to the FloatBuffer
-        vertexBuffer.put(triangleCoords);
+        vertexBuffer.put(pointCoords);
         // set the buffer to read the first coordinate
         vertexBuffer.position(0);
         // prepare shaders and OpenGL program
@@ -84,7 +81,22 @@ public class Triangle {
         GLES20.glAttachShader(mProgram, fragmentShader); // add the fragment shader to program
         GLES20.glLinkProgram(mProgram);                  // create OpenGL program executables
     }
+    public void SetVerts(float vx, float vy, float vz) {
+        pointCoords[0] = vx;
+        pointCoords[1] = vy;
+        pointCoords[2] = vz;
 
+        vertexBuffer.put(pointCoords);
+        // set the buffer to read the first coordinate
+        vertexBuffer.position(0);
+    }
+
+    public void SetColor(float red, float green, float blue, float alpha) {
+        color[0] = red;
+        color[1] = green;
+        color[2] = blue;
+        color[3] = alpha;
+    }
     /**
      * Encapsulates the OpenGL ES instructions for drawing this shape.
      *
@@ -114,7 +126,7 @@ public class Triangle {
         GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
         MyGLRenderer.checkGlError("glUniformMatrix4fv");
         // Draw the triangle
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 0, vertexCount);
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
