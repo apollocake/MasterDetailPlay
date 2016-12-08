00625dfd8211d46aaa5749471f6c3763800479c4
Afton Martin <aftonam@gmail.com>
2016-12-08 15:55:55 -0300

    got 3D points rotating around an axis

--- app/src/main/java/com/example/jack/masterdetailplay/MyGLRenderer.java
+++ app/src/main/java/com/example/jack/masterdetailplay/MyGLRenderer.java
@@ -73,16 +73,14 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // Use the following code to generate constant rotation.
         // Leave this code out when using TouchEvents.
          long time = SystemClock.uptimeMillis() % 4000L;
-        float angle = 0.090f * ((int) time);
-        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 0, 1.0f);
+        mAngle = 0.090f * ((int) time);
+        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 0, 1.0f, 0);
         // Combine the rotation matrix with the projection and camera view
         // Note that the mMVPMatrix factor *must be first* in order
         // for the matrix multiplication product to be correct.
-        Matrix.multiplyMM(mMVPMatrix, 0, mMVPMatrix, 0, mRotationMatrix, 0);
-        // Draw triangle
-        //mTriangle.draw(scratch);
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
         for (Point p:mPointList) {
-            p.draw(mMVPMatrix);
+            p.draw(scratch);
         }
     }
     @Override
